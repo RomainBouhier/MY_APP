9320df320dd6ca3f345a842f74eced7b1475ff14
romicron <roro.bouhier@gmail.com>
2019-11-20 15:24:09 +0100

    adding index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const PORT = process.env.PORT || 80;
+
+// Import library
+const express = require("express");
+
+// Create application
+const app = express();
+
+// Some basic route
+app.get("/", (req, res) => {
+  // Send response
+  res.json({ message: "coucou" });
+});
+
+// Start server
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.info("Server started.");
+});
